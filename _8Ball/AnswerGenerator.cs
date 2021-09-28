ae24712c8e478058f71e206a183f03f9f8807796
Nikolay Nikonorov <nnv@nikolay.moe>
2021-09-28 11:23:20 +0300

    done/Задание 5

---
+++ _8Ball/AnswerGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8Ball
+{
+    public class AnswerGenerator
+    {
+        private static string[] variants = { "да", "нет", "возможно", "маловероятно" };
+        private static Random random = new Random();
+        public static string GetRandomAnswer()
+        {
+            return variants[random.Next(0, variants.Length)];
+        }
+    }
+}
--- _8Ball/MainWindow.xaml.cs
+++ _8Ball/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,5 +25,22 @@ namespace _8Ball
         {
             InitializeComponent();
         }
+
+        // Обработчик события нажатия кнопки
+        private void Answer_Click(object sender, RoutedEventArgs e)
+        {
+            // Ставим курсор ожидания.
+            this.Cursor = Cursors.Wait;
+            // Делаем задержку, для создания эффекта того, что программа задумалась.
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            // Берем случайный ответ.
+            txtAnswer.Text = AnswerGenerator.GetRandomAnswer();
+            // Восстанавливаем прежнее состояние курсора.
+            this.Cursor = null;
+        }
+        private void txtQuestion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
     }
 }
